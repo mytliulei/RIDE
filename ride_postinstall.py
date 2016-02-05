f24b6ab7fd21b4fb4d7bd34a86638d424748b883
HelioGuilherme66 <helioxentric@gmail.com>
2016-02-05 01:29:24 +0000

    Changed Tk dialogs to wx, hopefully to fix crash in Windows.

--- ride_postinstall.py
+++ ride_postinstall.py
@@ -3,8 +3,6 @@
 
 import sys
 from os.path import exists, join
-from Tkinter import Tk
-from tkMessageBox import askyesno
 
 __doc__ = """
 Usage: ride_postinstall.py <-install|-remove>
@@ -26,9 +24,32 @@ http://sourceforge.net/projects/wxpython/files/wxPython/2.8.12.1/")
         print("Installation successful.")
 
 
+def _askyesno(title, message):
+    import wx
+    _ = wx.App()
+    parent = wx.Frame(None, size=(0, 0))
+    parent.CenterOnScreen()
+    dlg = wx.MessageDialog(parent, message, title, wx.YES_NO |
+                           wx.ICON_QUESTION)
+    result = dlg.ShowModal() == wx.ID_YES
+    dlg.Destroy()
+    return result
+
+
+def _askdirectory(title, initialdir):
+    import wx
+    _ = wx.App()
+    parent = wx.Frame(None, size=(0, 0))
+    dlg = wx.DirDialog(parent, title, initialdir, style=wx.DD_DIR_MUST_EXIST)
+    if dlg.ShowModal() == wx.ID_OK:
+        result = dlg.GetPath()
+    else: result = None
+    dlg.Destroy()
+    return result
+
+
 def _create_desktop_shortcut_linux():
     import os
-    Tk().withdraw()
     DEFAULT_LANGUAGE = os.environ.get('LANG', '').split(':')
     desktop = {"pt": r"Área de Trabalho", "en": "Desktop"}  # TODO: Add more\
                                                             # languages
@@ -37,21 +58,19 @@ def _create_desktop_shortcut_linux():
         link = join(os.path.join(os.path.expanduser('~'), ndesktop),
                     "RIDE.desktop")
     except KeyError as kerr:
-        from tkFileDialog import askdirectory
-        directory = askdirectory(title="Locate Desktop Directory",
-                                 mustexist=1,
+        directory = _askdirectory(title="Locate Desktop Directory",
                                  initialdir=os.path.join(os.path.expanduser('~'
                                                                             )))
         if not directory:
             sys.exit("Desktop shortcut creation aborted!")
         else:
             link = join(directory, "RIDE.desktop")
-    if exists(link) or askyesno("Setup", "Create desktop shortcut?"):
+    if exists(link) or _askyesno("Setup", "Create desktop shortcut?"):
         roboticon = "/usr/lib/python{0}/site-packages/robotide/widgets/robot.p\
 ng".format(sys.version[:3])
         with open(link, "w+") as shortcut:
-            shortcut.write("#!/usr/bin/env xdg-open\n[Desktop Entry]\nExec=ride.py\nComment=A Robot Fra\
-mework IDE\nGenericName=RIDE\n")
+            shortcut.write("#!/usr/bin/env xdg-open\n[Desktop Entry]\nExec=\
+ride.py\nComment=A Robot Framework IDE\nGenericName=RIDE\n")
             shortcut.write("Icon={0}\n".format(roboticon))
             shortcut.write("Name=RIDE\nStartupNotify=true\nTerminal=false\nTyp\
 e=Application\nX-KDE-SubstituteUID=false\n")
@@ -63,11 +82,10 @@ def _create_desktop_shortcut_mac():
 
 
 def _create_desktop_shortcut_windows():
-    Tk().withdraw()
     link = join(get_special_folder_path("CSIDL_DESKTOPDIRECTORY"), 'RIDE.lnk')
     icon = join(sys.prefix, 'Lib', 'site-packages', 'robotide', 'widgets',
                 'robot.ico')
-    if exists(link) or askyesno('Setup', 'Create desktop shortcut?'):
+    if exists(link) or _askyesno('Setup', 'Create desktop shortcut?'):
         create_shortcut('pythonw', "Robot Framework testdata editor", link,
                         '-c "from robotide import main; main()"', '', icon)
         file_created(link)
@@ -87,7 +105,7 @@ def create_desktop_shortcut():
 
 
 if len(sys.argv) > 1 and sys.argv[1] == '-install':
-    # verify_install()
+    verify_install()
     create_desktop_shortcut()
 else:
     print(__doc__)
